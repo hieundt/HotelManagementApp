12311e6a11c4e51c5a51c9ce94c8e07e81aee80e
Thương Lê Thị <21521509@ms.uit.edu.vn>
2022-12-25 23:36:58 +0700

    re update

--- HotelManagementApp/ViewModel/Settings/SettingsViewModel.cs
+++ HotelManagementApp/ViewModel/Settings/SettingsViewModel.cs
@@ -20,6 +20,29 @@ namespace HotelManagementApp.ViewModel
         public string NewPassword { get => _NewPassword; set { _NewPassword = value; OnPropertyChanged(); } }
         private string _ConfirmPassword;
         public string ConfirmPassword { get => _ConfirmPassword; set { _ConfirmPassword = value; OnPropertyChanged(); } }
+
+        private int _BookingID;
+        public int Booking_Id { get => _BookingID; set { _BookingID = value; OnPropertyChanged(); } }
+        private string _CustomerName;
+        public string CustomerName { get => _CustomerName; set { _CustomerName = value; OnPropertyChanged(); } }
+        private string _PhoneNum;
+        public string CustomerPhoneNumber { get => _PhoneNum; set { _PhoneNum = value; OnPropertyChanged(); } }
+        private DateTime? _StartTime = null;
+        public DateTime? CheckInTime { get => _StartTime; set { _StartTime = value; OnPropertyChanged(); } }
+        private DateTime? _DateBooking = null;
+        public DateTime? DateBooking { get => _DateBooking; set { _DateBooking = value; OnPropertyChanged(); } }
+        private DateTime? _EndTime = null;
+        public DateTime? CheckOutTime { get => _EndTime; set { _EndTime = value; OnPropertyChanged(); } }
+        private double _GoodTotal;
+        public double Good_total { get => _GoodTotal; set { _GoodTotal = value; OnPropertyChanged(); } }
+        private double _RoomPrice;
+        public double Room_price { get => _RoomPrice; set { _RoomPrice = value; OnPropertyChanged(); } }
+        private double _TotalPayMent;
+        public double Total_Payment { get => _TotalPayMent; set { _TotalPayMent = value; OnPropertyChanged(); } }
+
+       
+        public ICommand OpenBillReportWindow { get; set; }
+
         public ICommand LogoutCommand { get; set; }
         public ICommand ChangePasswordCommand { get; set; }
 
@@ -55,6 +78,48 @@ namespace HotelManagementApp.ViewModel
                 CurrentPassword = NewPassword = ConfirmPassword = null;
             }
             );
+
+            OpenBillReportWindow = new RelayCommand<object>((p) => { return true; }, (p) =>
+            {
+                var room = new RoomsReservation();
+                room = DataProvider.Instance.DB.RoomsReservations.Where(x => x.ID == room.ID).FirstOrDefault();
+                Booking_Id = room.ID;
+                CheckInTime = room.CheckInTime;
+                CheckOutTime = room.CheckOutTime;
+
+                var customer = new Customer();
+                customer = DataProvider.Instance.DB.Customers.Where(x => x.ID == customer.ID).FirstOrDefault();
+                CustomerName = customer.Name;
+                CustomerPhoneNumber = customer.PhoneNumber;
+
+                var roomType = new RoomType();
+                roomType = DataProvider.Instance.DB.RoomTypes.Where(x => x.ID == roomType.ID).FirstOrDefault();
+                Room_price = (double)roomType.Price;
+
+                var order = new Order();
+                order = DataProvider.Instance.DB.Orders.Where(x => x.ID == order.ID).FirstOrDefault();
+                Good_total = (double)order.TotalPrice;
+
+                var bill = new BillDetail();
+                bill = DataProvider.Instance.DB.BillDetails.Where(x => x.ID == bill.ID).FirstOrDefault();
+                Total_Payment = (double)bill.TotalMoney;
+
+                BillReportViewModel vm = new BillReportViewModel();
+                vm.Booking_Id = Booking_Id;
+                vm.DateBooking = DateBooking;
+                vm.CheckInTime = CheckInTime;
+                vm.CheckOutTime = CheckOutTime;
+                vm.CustomerName = CustomerName;
+                vm.CustomerPhoneNumber = CustomerPhoneNumber;
+                vm.Good_total = Good_total;
+                vm.Room_price = Room_price;
+                vm.Total_Payment = "Total payment: " + Total_Payment.ToString();
+
+                Window paymentWindow = new PaymentWindow();
+                paymentWindow.DataContext = vm;
+                paymentWindow.Show();
+            }
+          );
         }
 
         private string MD5Hash(string input)
